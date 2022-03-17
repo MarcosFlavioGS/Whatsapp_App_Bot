ba6bc10a9d32102b170772af27f3cfe1f52046f5
Marcos Flavio <mfghost69@gmail.com>
2022-03-17 17:29:29 -0300

    incluindo função de envio de email

--- BotApp.py
+++ BotApp.py
@@ -2,6 +2,11 @@
 import pyautogui
 import time
 import pyperclip #Precisei porque o pyautogui é uma porcaria para escrever caracteres especiais
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+# Mover o mouse para o canto superior esquerdo para parar o processo
 pyautogui.FAILSAFE = True
 
 #Abrir arquivos
@@ -18,17 +23,45 @@ with open("filepath.txt", "r") as filepath_txt:
     for i in filepath_txt:
         filepath.append(i)
 
-
+#Abrir arquivo de mensagem
 with open("mensagem.txt", "r", encoding="UTF-8") as mensagem_txt:
     mensagem = []
     for i in mensagem_txt:
-        mensagem.append(i)  
+        mensagem.append(i)
 
+# Abrir arquivo de mensagem do email
+with open("mensagem_email.txt", "r", encoding="UTF-8") as mensagememail_txt:
+    emailMessage1 = []
+    for i in mensagememail_txt:
+        emailMessage1.append(i)
+    emailMessage = ''.join(emailMessage1)
+
+#Abrir arquivo de titulo de email
+with open("titulo_email.txt", "r", encoding="UTF-8") as titulo_txt:
+    titulo1 = []
+    for i in titulo_txt:
+        titulo1.append(i)
+    titulo = ''.join(titulo1)    
+
+# Abrir arquivo de senha
+with open("senha_email.txt", "r", encoding="UTF-8") as password_txt:
+    secret1 = []
+    for i in password_txt:
+        secret1.append(i)
+    secret = ''.join(secret1)    
+
+# Abrir arquivo de email
+with open("email.txt", "r", encoding="UTF-8") as email_txt:
+    useremail1 = []
+    for i in email_txt:
+        useremail1.append(i)
+    useremail = ''.join(useremail1)
+#Inicio
 print("********************************************************************************")
 print("**** O programa vai iniciar em 10 segundos... ")
 print("**** Por favor não usar movimentar o mouse nem usar o teclado durante a execução")
 print("********************************************************************************")
-time.sleep(1)
+time.sleep(10)
 
 #Abrir whatsapp_app
 pyautogui.press('winleft')
@@ -37,7 +70,7 @@ pyautogui.press('Enter')
 time.sleep(10)
 
 
-#Procurar contato
+#Procurar contato no whatsapp
 def busca_contato(contato):
     print(contato)
     pyautogui.moveTo(x=142, y=114)
@@ -47,15 +80,17 @@ def busca_contato(contato):
     pyautogui.press('enter')
     time.sleep(2)
 
-    #Mandar imagem
+#Mandar imagem
 def mandar_mensagem(filepath, mensagem):
+    
     pyautogui.moveTo(x=504, y=830)
     pyautogui.click()
     time.sleep(1)
     pyautogui.press('down')
     pyautogui.press('enter')
     time.sleep(3)
-    for i in filepath:
+    
+    for i in filepath:  
         pyautogui.write(i)
         pyautogui.press('enter')
         time.sleep(1)    
@@ -71,26 +106,64 @@ def mandar_mensagem(filepath, mensagem):
         pyautogui.press('enter')
         time.sleep(1)
 
-
+#Função condicional para busca de contato não cadastrado via chrome
 def busca_online(contato):
     pyautogui.press('winleft')
     pyautogui.write('Chrome')
     pyautogui.press('enter')
     time.sleep(5)
-    #pyautogui.write(f"https://api.whatsapp.com/send?phone=55{contato}") Outra possibilidade
-    pyperclip.copy(contato)
+    pyperclip.copy("https://api.whatsapp.com/send?phone=")
     pyautogui.hotkey("ctrl", "v")
+    pyautogui.write(contato)
     pyautogui.press('enter')
     time.sleep(5)
 
+#Função para envio de email ao invés de whatsapp caso o contato seja um email
+def email(useremail, contato, emailMessage, titulo, secret):
+    subject = f"{titulo}"
+    body = f"{emailMessage}"
+    sender_email = f"{useremail}"
+    receiver_email = f"{contato[1:]}"
+    password = f"{secret}" #Aqui é o local de senha do email. Porém, é melhor usar algum tipo de arquivo criptografado
 
-for contato in contatos:
+    message = EmailMessage()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
 
-    if contato[:4] == "http":
+    #Formatação html
+    html = f"""
+    <html>
+        <body>
+            <h1>{subject}</h1>
+            <p>{body}</p>
+        </body>
+    </html>
+    """
+
+    message.add_alternative(html, subtype="html")
+
+    context = ssl.create_default_context()
+
+    print("ENVIANDO EMAIL!")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    print("Email enviado")
+
+for contato in contatos: #Chamando funções
+
+    if contato[:2] == "55":
         print("Buscando contato online")
 
         busca_online(contato)
         mandar_mensagem(filepath, mensagem)
+    
+    elif contato[:1] == "@":
+        email(useremail, contato, emailMessage, titulo, secret)
+    
     else:
         print("Executando Bot")
         try:
--- README.md
+++ README.md
@@ -6,3 +6,19 @@ O programa também possui uma checagem de exceções para que não pare de funci
 usuário possa verificar, posteriormente, algum possível erro de digitação.
 
 Esse Bot ainda está em processo de aprimoramento e será atualizado no futuro.
+
+-- Utilização dos arquivos .txt de consulta:
+
+    - contatos.txt:
+        -Aqui deve-se colocar por escrito os contatos um por linha, identificando o tipo de contato com as "tags" apropriadas: 
+            - Contatos já cadastrados nos contatos de whatsapp do usuário devem ser escritos EXATAMENTE como estão escritos na sua lista de contatos no whatsapp, porém, sem a acentuação.
+
+            - Contatos não cadastrados em sua lista de contatos do whatsapp poderam ser passados apenas com seus números de celular, basta que seja passado na frente do contato o número '55' seguidos pelo restante do número: 55XXXXXXXXXXX.
+
+            -   Contatos cuja única informação para contato seja um email também poderão ser acessados, basta escrever o email antecedido por um '@', ficando dessa forma: @esteeumexemplo@gmail.com. 
+             * É importante ficar claro que o email DESTINATÁRIO poderá ser de qualquer servidor de email sem nenhuma restrição, porém, o usuário deste programa só poderá enviar emails a partir de um @GMAIL.*
+    - filepath.txt:
+        -Aqui será especificado o caminho em sua maquina onde se encontra a imagem a ser enviada pelo whatsapp.
+
+    - mensagem.txt:
+        Escreva aqui a mensagem a ser enviada para seus contatos.    
